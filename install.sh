0fe2cf53da366c05f81de69c06700e4b1b186760
Travis Weir <tweirtx@gmail.com>
2020-01-08 15:35:06 -0600

    Start TravisCI

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+npm i -g pm2 gulp-cli
+cd audience-display && npm install && cd ..
+cd ems-api && npm install && cd ..
+cd ems-core && npm install && cd ..
+cd ems-home && npm install && cd ..
+cd ems-socket && npm install && cd ..
+cd ems-web && npm install && cd ..
+cd ref-tablet && npm install && cd ..
+npm install

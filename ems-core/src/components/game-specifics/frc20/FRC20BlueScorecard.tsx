3545f25c3a81d9432b8cd1e2eae43b6d017a4762
techno11 <soren.zaiser@gmail.com>
2020-01-20 16:39:19 -0500

    Tenitively Finish Scorecards

--- ems-core/src/components/game-specifics/frc20/FRC20BlueScorecard.tsx
+++ ems-core/src/components/game-specifics/frc20/FRC20BlueScorecard.tsx
@@ -31,6 +31,9 @@ class FRC20BlueScorecard extends React.Component<IProps> {
     this.modifyRobotOneCrossed = this.modifyRobotOneCrossed.bind(this);
     this.modifyRobotTwoCrossed = this.modifyRobotTwoCrossed.bind(this);
     this.modifyRobotThreeCrossed = this.modifyRobotThreeCrossed.bind(this);
+    this.modifyRobotOneEndgame = this.modifyRobotOneEndgame.bind(this);
+    this.modifyRobotTwoEndgame = this.modifyRobotTwoEndgame.bind(this);
+    this.modifyRobotThreeEndgame = this.modifyRobotThreeEndgame.bind(this);
     this.modifyAutoBottomCells = this.modifyAutoBottomCells.bind(this);
     this.modifyAutoOuterCells = this.modifyAutoOuterCells.bind(this);
     this.modifyAutoInnerCells = this.modifyAutoInnerCells.bind(this);
@@ -39,6 +42,13 @@ class FRC20BlueScorecard extends React.Component<IProps> {
     this.modifyTeleInnerCells = this.modifyTeleInnerCells.bind(this);
     this.modifyRotationControl = this.modifyRotationControl.bind(this);
     this.modifyPositionControl = this.modifyPositionControl.bind(this);
+    this.modifyStageOneCells = this.modifyStageOneCells.bind(this);
+    this.modifyStageTwoCells = this.modifyStageTwoCells.bind(this);
+    this.modifyStageThreeCells = this.modifyStageThreeCells.bind(this);
+    this.modifyStage = this.modifyStage.bind(this);
+    this.modifyBalanced = this.modifyBalanced.bind(this);
+    this.modifyMajorPenalties = this.modifyMajorPenalties.bind(this);
+    this.modifyMinorPenalties = this.modifyMinorPenalties.bind(this);
   }
 
   public render() {
@@ -56,8 +66,8 @@ class FRC20BlueScorecard extends React.Component<IProps> {
             <Grid className="details">
               <Grid.Row columns="equal" textAlign="center">
                 <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Auto Robot 1 Crossed" checked={details.blueAutoRobotOneCrossed} onChange={this.modifyRobotOneCrossed}/></Grid.Column>
-                <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Auto Robot 3 Crossed" checked={details.blueAutoRobotThreeCrossed} onChange={this.modifyRobotTwoCrossed}/></Grid.Column>
-                <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Auto Robot 2 Crossed" checked={details.blueAutoRobotTwoCrossed} onChange={this.modifyRobotThreeCrossed}/></Grid.Column>
+                <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Auto Robot 2 Crossed" checked={details.blueAutoRobotTwoCrossed} onChange={this.modifyRobotTwoCrossed}/></Grid.Column>
+                <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Auto Robot 3 Crossed" checked={details.blueAutoRobotThreeCrossed} onChange={this.modifyRobotThreeCrossed}/></Grid.Column>
               </Grid.Row>
               <Grid.Row columns="equal" textAlign="center">
                 <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Auto Bottom Cells" value={details.blueAutoBottomCells} onChange={this.modifyAutoBottomCells}/></Grid.Column>
@@ -71,10 +81,30 @@ class FRC20BlueScorecard extends React.Component<IProps> {
           <Form>
             <Grid className="details">
               <Grid.Row columns="equal" textAlign="center">
-                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Bottom Cells" value={details.blueAutoBottomCells} onChange={this.modifyAutoBottomCells}/></Grid.Column>
-                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Outer Cells" value={details.blueAutoOuterCells} onChange={this.modifyAutoOuterCells}/></Grid.Column>
-                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Inner Cells" value={details.blueAutoInnerCells} onChange={this.modifyAutoInnerCells}/></Grid.Column>
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Bottom Cells" value={details.blueAutoBottomCells} onChange={this.modifyTeleBottomCells}/></Grid.Column>
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Outer Cells" value={details.blueAutoOuterCells} onChange={this.modifyTeleOuterCells}/></Grid.Column>
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Tele Inner Cells" value={details.blueAutoInnerCells} onChange={this.modifyTeleInnerCells}/></Grid.Column>
               </Grid.Row>
+            </Grid>
+          </Form>
+        </Card.Content>
+        <Card.Content>
+          <Form>
+            <Grid className="details">
+              <Grid.Row columns="equal" textAlign="center">
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Stage 1 Cells" value={details.blueStageOneCells} onChange={this.modifyStageOneCells}/></Grid.Column>
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Stage 3 Cells" value={details.blueStageTwoCells} onChange={this.modifyStageTwoCells}/></Grid.Column>
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Stage 3 Cells" value={details.blueStageThreeCells} onChange={this.modifyStageThreeCells}/></Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal" textAlign="center">
+                <Grid.Column className="align-bottom"><Form.Input disabled={disabled} fluid={true} label="Stage" value={details.blueStage} onChange={this.modifyStage}/></Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Form>
+        </Card.Content>
+        <Card.Content>
+          <Form>
+            <Grid className="details">
               <Grid.Row columns="equal" textAlign="center">
                 <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Rotation Control" checked={details.blueRotationControl} onChange={this.modifyRotationControl}/></Grid.Column>
                 <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Position Control" checked={details.bluePositionControl} onChange={this.modifyPositionControl}/></Grid.Column>
@@ -86,9 +116,12 @@ class FRC20BlueScorecard extends React.Component<IProps> {
           <Form>
             <Grid className="details">
               <Grid.Row columns="equal" textAlign="center">
-                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 1 Endgame" value={details.blueEndRobotOneStatus} options={DropdownData.RoverRuckusEndItems} onChange={this.modifyRobotOneEndgame}/></Grid.Column>
-                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 2 Endgame" value={details.blueEndRobotTwoStatus} options={DropdownData.RoverRuckusEndItems} onChange={this.modifyRobotTwoEndgame}/></Grid.Column>
-                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 3 Endgame" value={details.blueEndRobotThreeStatus} options={DropdownData.RoverRuckusEndItems} onChange={this.modifyRobotThreeEndgame}/></Grid.Column>
+                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 1 Endgame" value={details.blueEndRobotOneStatus} options={DropdownData.InfiniteRechargeEndItems} onChange={this.modifyRobotOneEndgame}/></Grid.Column>
+                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 2 Endgame" value={details.blueEndRobotTwoStatus} options={DropdownData.InfiniteRechargeEndItems} onChange={this.modifyRobotTwoEndgame}/></Grid.Column>
+                <Grid.Column><Form.Dropdown disabled={disabled} fluid={true} label="Robot 3 Endgame" value={details.blueEndRobotThreeStatus} options={DropdownData.InfiniteRechargeEndItems} onChange={this.modifyRobotThreeEndgame}/></Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns="equal" textAlign="center">
+                <Grid.Column><Form.Checkbox disabled={disabled} fluid={true} label="Balanced?" checked={details.blueEndEqualized} onChange={this.modifyBalanced}/></Grid.Column>
               </Grid.Row>
             </Grid>
           </Form>
@@ -196,6 +229,12 @@ class FRC20BlueScorecard extends React.Component<IProps> {
     this.forceUpdate();
   }
 
+  private modifyBalanced(event: SyntheticEvent, props: CheckboxProps) {
+    (this.props.details as InfiniteRechargeMatchDetails).blueEndEqualized = props.checked;
+    this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
+    this.forceUpdate();
+  }
+
   private modifyRobotOneEndgame(event: SyntheticEvent, props: DropdownProps) {
     (this.props.details as InfiniteRechargeMatchDetails).blueEndRobotOneStatus = parseInt(props.value + "", 10);
     this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
@@ -210,6 +249,30 @@ class FRC20BlueScorecard extends React.Component<IProps> {
 
   private modifyRobotThreeEndgame(event: SyntheticEvent, props: DropdownProps) {
     (this.props.details as InfiniteRechargeMatchDetails).blueEndRobotThreeStatus = parseInt(props.value + "", 10);
+    this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
+    this.forceUpdate();
+  }
+
+  private modifyStageOneCells(event: SyntheticEvent, props: DropdownProps) {
+    (this.props.details as InfiniteRechargeMatchDetails).blueStageOneCells = parseInt(props.value + "", 10);
+    this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
+    this.forceUpdate();
+  }
+
+  private modifyStageTwoCells(event: SyntheticEvent, props: DropdownProps) {
+    (this.props.details as InfiniteRechargeMatchDetails).blueStageTwoCells = parseInt(props.value + "", 10);
+    this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
+    this.forceUpdate();
+  }
+
+  private modifyStageThreeCells(event: SyntheticEvent, props: DropdownProps) {
+    (this.props.details as InfiniteRechargeMatchDetails).blueStageThreeCells = parseInt(props.value + "", 10);
+    this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
+    this.forceUpdate();
+  }
+
+  private modifyStage(event: SyntheticEvent, props: DropdownProps) {
+    (this.props.details as InfiniteRechargeMatchDetails).blueStage = parseInt(props.value + "", 10);
     this.props.match.blueScore = this.props.details.getBlueScore(this.props.match.redMinPen, this.props.match.redMajPen);
     this.forceUpdate();
   }

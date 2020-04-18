5f7ae10924786de692ced7493b61dc58b30622b3
Travis Weir <tweirtx@gmail.com>
2020-04-18 18:18:45 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,5 +3,8 @@ cd build/ems
 npm install sqlite3 --build-from-source --runtime=electron --target=2.0.4 --dist-url=https://atom.io/download/electron
 npm i --production
 cd ../..
+gulp post-build
+gulp update-pkg
+npm run predist
 npm run dist
 ls
